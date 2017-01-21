401ec16b350f2ddfb4c4f0728ce130b738ba8de9
Alequin <j-cox03@hotmail.com>
2017-01-21 11:23:42 +0000

    background colour fade in working

--- js/colour-wheel.js
+++ js/colour-wheel.js
@@ -1,13 +1,24 @@
+var $innerBody;
+
+$(document).ready(function() {
+
+  $innerBody = $('#inner-body');
+
+  $('.colour-circle').on('mouseenter', function(){
+    var colourToSet = $(this).css("background-color");
+    fadeBackgroundIn(colourToSet);
+  });
+});
 
 function onClickEnter(){
   $('#intro-container').fadeOut(2000);
   $('#main-container').fadeIn(2000);
 }
 
-$(document).ready(function() {
-
-  $('.colour-circle').on('mouseenter', function(){
-    var colourToSet = $(this).css("background-color");
-    $('body').css("background-color", colourToSet);
-  });
-});
+function fadeBackgroundIn(colourToSet){
+    var previousInnerBodyColour = $innerBody.css("background-color");
+    $('body').css("background-color", previousInnerBodyColour);
+    $innerBody.css("display", "none");
+    $innerBody.css("background-color", colourToSet);
+    $innerBody.fadeIn(500);
+}
